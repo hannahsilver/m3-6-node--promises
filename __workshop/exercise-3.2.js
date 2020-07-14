2606e088ea81894781a9fc23977ad264602e3a9e
Hannah Silver <hannah_silver@hotmail.com>
2020-07-13 23:21:01 -0400

    exercise 3.2

--- __workshop/exercise-3.2.js
+++ __workshop/exercise-3.2.js
@@ -1,18 +1,26 @@
 // Exercise 3.2 - `getAddressPosition`
 // ---------------------------------
 
-const opencage = require('opencage-api-client');
-require('dotenv').config();
+const opencage = require("opencage-api-client");
+require("dotenv").config();
+const api_key = process.env.OPENCAGE_API_KEY;
 
 const getPositionFromAddress = (address) => {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+    key: api_key,
+    q: address,
   };
 
-  // return something...
+  return opencage.geocode(requestObj).then((data) => {
+    data = data.results[0].geometry;
+    return data;
+  });
 };
 
 getPositionFromAddress(
-  '1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8'
+  "1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8"
+).then((response) => console.log(response));
+
+getPositionFromAddress(
+  "11 Villeneuve E, Montréal, QC H2T 1K7"
 ).then((response) => console.log(response));
