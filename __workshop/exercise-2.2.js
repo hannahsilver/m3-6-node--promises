75262976377613b03e4424bc3e76163aaf81e92c
Hannah Silver <hannah_silver@hotmail.com>
2020-07-13 23:19:23 -0400

    exercise 2,2

--- __workshop/exercise-2.2.js
+++ __workshop/exercise-2.2.js
@@ -2,18 +2,21 @@
 // ----------------------
 
 // require the 'request-promise' module.
-const request = require('request-promise');
+const request = require("request-promise");
 
 const greeting = (langCode) => {
-  return request('<URL>') // 1
-    .then((response) => JSON.parse(response))
-    .then((parsedResponse) => {
-      return; // 2
+  return request(`https://journeyedu.herokuapp.com/hello/${langCode}`) // 1
+    .then((response) => {
+      return JSON.parse(response);
     })
-    .catch((err) => console.log('Error: ', err));
+    .then((parsedResponse) => {
+      return parsedResponse.data; // 2
+    })
+    .catch((err) => console.log("Error: ", err));
 };
 
 // Testing
-greeting('fr').then((result) => console.log(result)); // { lang: "French", code: "FR", text: "Bonjour" }
+greeting("fr").then((result) => console.log(result)); // { lang: "French", code: "FR", text: "Bonjour" }
 
 // 3
+greeting("de").then((result) => console.log(result));
