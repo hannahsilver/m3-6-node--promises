64fec7917c7773f3df2b9070a85f3fe1bb007d20
Hannah Silver <hannah_silver@hotmail.com>
2020-07-13 23:19:42 -0400

    exercise 2.3

--- __workshop/exercise-2.3.js
+++ __workshop/exercise-2.3.js
@@ -2,6 +2,22 @@
 // ----------------------------
 
 // require the 'request-promise' module.
-const request = require('request-promise');
+
+const request = require("request-promise");
 
 // get the code you wrote in 2.2 and paste it here...
+
+const greeting = (langCode) => {
+  return request(`https://journeyedu.herokuapp.com/hello/${langCode}`) // 1
+    .then((response) => {
+      return JSON.parse(response);
+    })
+    .then((parsedResponse) => {
+      return parsedResponse.data; // 2
+    })
+    .catch((err) => {
+      return err.error ? JSON.parse(err.error) : err;
+    });
+};
+
+greeting("hello").then((result) => console.log(result)); //
