07f80c58fce3e49507845b5f3b58ca4df5bbc4a7
Hannah Silver <hannah_silver@hotmail.com>
2020-07-13 23:20:18 -0400

    exercise 3.1

--- __workshop/exercise-3.1.js
+++ __workshop/exercise-3.1.js
@@ -2,8 +2,23 @@
 // -----------------------------
 
 // require the 'request-promise' module.
+const request = require("request-promise");
 
 // Returns the current position of the ISS
 const getIssPosition = () => {
   // write some code...
+  return request("http://api.open-notify.org/iss-now.json")
+    .then((response) => {
+      const data = JSON.parse(response);
+      return data;
+    })
+    .then((data) => {
+      const newPositions = {
+        lat: data.iss_position.latitude,
+        lng: data.iss_position.longitude,
+      };
+      return newPositions;
+    });
 };
+
+getIssPosition().then((result) => console.log(result));
